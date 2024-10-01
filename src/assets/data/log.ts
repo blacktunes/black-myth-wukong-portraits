ab11f5097338d3cc843fc81ab2277a44329f994d
blacktunes <blacktunes@foxmail.com>
2024-10-01 17:31:14 +0800

    chore: 添加更新记录

--- src/assets/data/log.ts
+++ src/assets/data/log.ts
@@ -2,6 +2,23 @@ import type { LogData } from 'star-rail-vue'
 
 const log: LogData[] = [
   {
+    time: '2024-10-01',
+    text: [
+      {
+        text: '优化界面和操作',
+        info: ['默认显示游戏里的四个分类','增加新建影神图时分类选择页', '增加删除快捷键(Del)']
+      },
+      
+      {
+        text: '优化动画效果',
+      },
+      {
+        text: '增加影神图的管理菜单',
+        info: '选择影神图时右键(移动端长按)可打开'
+      }
+    ]
+  },
+  {
     time: '2024-09-29',
     text: [
       {
